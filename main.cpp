6ba619a0c51f85990f202d72292a28e7737c513b
Walter Aoiama Nagai <walternagai@unifei.edu.br>
2021-05-24 22:26:37 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,17 +2,6 @@
 using namespace std;
 
 int main() {
-  // crie as variáveis valorbruto e aliquota
-  
-  // crie uma variável valorreal
-  
-  // leia o valorbruto
-  
-  // leia a aliquota
-  
-  // calcule o valorreal
-  
-  // mostre o valor de valorreal
   
   return 0;
 }
